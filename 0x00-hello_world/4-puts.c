32f091360d1d0fa0c0b632bf250e94dfd64ba616
omuabormalin <omuabormalin@gmail.com>
2022-09-08 14:53:55 -0700

    added a comment to describe the function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 int main(void)
+/**
+ * *main - bla bla
+ * *Description: main xyz
+ * *Return: 0 (success)
+ * */
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
