0d092c0f524253260bd5acf796eb009dfeeacc4f
omuabormalin <omuabormalin@gmail.com>
2022-09-08 13:13:28 -0700

    edited the 4-puts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
-int main(void);
-	{
 
-		puts("\"Programming is like building a multilingual puzzle");
 
-			return (0);
 
-	}
+/**
+ *
+ *  * main - Entry point
+ *
+ *   * Return: Always 0 (Success)
+ *
+ *    */
+
+
+
+int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}
