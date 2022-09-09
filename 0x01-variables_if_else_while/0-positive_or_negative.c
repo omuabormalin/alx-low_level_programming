08e8babe36718c8467455c8d5a17684524b9e2f6
omuabormalin <omuabormalin@gmail.com>
2022-09-09 09:27:44 -0700

    n is zero corrected

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,7 +15,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	if (n == 0)
-		printf("%d is negative\n", n);
+		printf("%d is zero\n", n);
 	else if (n < 0)
 		printf("%d is negative\n", n);
 	else
