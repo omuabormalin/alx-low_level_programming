df9257a7e4e2a064089cfdc0d3551466aa0c5ca4
omuabormalin <omuabormalin@gmail.com>
2022-09-09 10:21:57 -0700

    created the file 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h?
+
+/**
+*main - holds my main codes
+*Return: 0 mean code is (success)
+*/
+
+int main(void)
+{
+	char y;
+
+	for (y = 'a'; y <= 'z'; y++)
+	for (s = 'A'; s <= 'Z'; s++)
+	putchar(y);
+	putchar(s);
+	putchar('\n');
+	return (0);
+}
