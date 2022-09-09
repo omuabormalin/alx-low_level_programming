4e0649707879f9d3f96a2e0e5ad38204c4aefa9a
omuabormalin <omuabormalin@gmail.com>
2022-09-09 12:17:29 -0700

    copied the code in file 5 to file 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - this is to print single base 10 digits
+* Return: 0 (success)
+*/
+
+int main(void)
+
+{
+int a;
+
+for (a = 0; a <= 9; a++)
+printf("%i", a);
+putchar('\n');
+return (0);
+}
