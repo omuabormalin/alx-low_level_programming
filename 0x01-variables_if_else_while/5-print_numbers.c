f1156169eed651f47b1e502c9ff09c1ad73b1d03
omuabormalin <omuabormalin@gmail.com>
2022-09-09 12:00:52 -0700

    created the ./5-print_numbers file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - this is to print single base 10 digits
+* Return: 0 (success)
+*/
+
+int main(void)
+{
+int a;
+for (a = 0 ; a <= 9 ; a)
+printf('a');
+return (0);
+}
