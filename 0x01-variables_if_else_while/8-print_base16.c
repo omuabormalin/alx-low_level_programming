f2fd0724299296c43c1d150d480d9a43e1131a97
omuabormalin <omuabormalin@gmail.com>
2022-09-09 13:23:42 -0700

    created the file 8-print_base16.c and wrote codes inside

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - the will print hexadecimalks in lower case
+* Return: will be 0 (success)
+*/
+
+int main(void)
+{
+int a;
+char s;
+for (a = 0; a <= 9; a++)
+for (s = 'a' ; s <= 'f' s++)
+putchar(a + 0);
+putchar('s');
+putchar('\n');
+return (0);
+}
