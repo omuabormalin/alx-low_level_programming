b28c4d35946d3c9b6ba605b976d5237d5ea4a593
omuabormalin <omuabormalin@gmail.com>
2022-09-09 12:43:24 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 
 /**
-* main - this is to print single base 10 digits
-* Return: a 0 (success)
+* main - this is to print single base digits
+* Return: Always 0 (Success)
 */
-
 int main(void)
 
 {
 int a;
 
 for (a = 0; a < 10; a++)
-putchar(a + 0);
+putchar(a + '0');
 putchar('\n');
 return (0);
 }
