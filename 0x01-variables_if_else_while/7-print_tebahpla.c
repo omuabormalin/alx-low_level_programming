d7e930282532284e85442b0a6ecb774ffc744e86
omuabormalin <omuabormalin@gmail.com>
2022-09-09 12:53:03 -0700

    created 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - to print reverse abc
+* Return: will be 0 (success)
+*/
+
+int main(void)
+{
+char s;
+for (s = 'z' ; s => 'a' ' s++)
+putchar(s);
+putchar('\n');
+return (0);
+}
