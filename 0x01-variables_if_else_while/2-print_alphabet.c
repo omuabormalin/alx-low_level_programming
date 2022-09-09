fded4d3b7c548e43e838842c28cdb2b81ab09d52
omuabormalin <omuabormalin@gmail.com>
2022-09-09 09:58:57 -0700

    new file created 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+* main - this is my entry poin
+* return: 0 (success)
+*/
+
+int main(void);
+
+{
+	char m;
+
+	for (s = 'a'; s <= 'z'; s++)
+	putchar(s);
+	putchar('\n')
+		return (0);
+}
