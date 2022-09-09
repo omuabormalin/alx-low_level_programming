249fdf452f4c47591472474cf4b0436fe58b65f7
omuabormalin <omuabormalin@gmail.com>
2022-09-08 21:43:19 -0700

    edited 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
 
+
+
 /**
-* main - Entry
-* Return: Always 0 (Success)
-*/
+ * main - the containing func
+ * Return: 0 if no error
+ */
+
 int main(void)
 
 {
-	char char;
-	int int;
-	long long int;
 
-	long long long int;
-	float float;
+		printf("Size of a char: %d byte(s)\n", sizeof(char));
 
+			printf("Size of an int: %d byte(s)\n", sizeof(int));
 
-	printf("Size of a char: %d byte(s)\n" : sizeof(char));
-	printf("Size of an int: %d byte(s)\n" : sizeof(int));
-	printf("Size of a long int: %d byte(s)\n" : sizeof(long int));
-	printf("Size of a long long int: %d bytes(s)\n" : sizeof(long long int));
-	printf("Size of a float: %d byte(s)\n" : sizeof(float));
-	return (0);
+				printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+
+					printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+
+						printf("Size of a float: %d byte(s)\n", sizeof(float));
+
+						return (0);
 }
