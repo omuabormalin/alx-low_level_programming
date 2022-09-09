6dc076ef64e77acdb09d91b80eb710352a73db36
omuabormalin <omuabormalin@gmail.com>
2022-09-09 13:47:29 -0700

    created a file with code in it

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - print single digits
+* Return: will be 0 (succes)
+*/
+
+int main(void)
+{
+int a;
+for (a = 0; a < 10; a++)
+putchar(a + 0);
+if (a > 0)
+{
+putchar(',');
+putchar(' ')'
+}
+putchar('\n');
+return (0);
+}
