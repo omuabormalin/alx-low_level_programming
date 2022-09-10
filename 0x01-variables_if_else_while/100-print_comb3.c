15a36454a47e44bdab56ded87ac0301c056d23c6
omuabormalin <omuabormalin@gmail.com>
2022-09-10 03:36:58 -0700

    edited the file code

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,11 +11,13 @@ int a;
 int b;
 for (a = 0; a <= 9; a++)
 {
-for (b = 1; b <= 8; b++)
+for (b = 1; b <= 9; b++)
 {
 putchar(a + '0');
 putchar(b + '0');
+
 if (!(a == 9 && b == 9))
+
 {
 putchar(',');
 putchar(' ');
