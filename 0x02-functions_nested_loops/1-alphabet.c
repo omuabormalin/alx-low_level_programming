790a8da3f91d0ab7b5556472cbe4e4b6b680ccf6
omuabormalin <omuabormalin@gmail.com>
2022-09-13 08:10:15 -0700

    print lower case file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - this is to print alphabets in lower cases
+* Return: will be 0 (success)
+*/
+
+int void print_alphabet(void);
+{
+char s;
+for (s = "a" ; s <= "z" ; s++)
+{
+putchar("%s"\n, s);
+}
+return (0);
+}
