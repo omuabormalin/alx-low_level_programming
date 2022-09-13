f612467d7943b3c06af07d0c6694792742d98c0d
omuabormalin <omuabormalin@gmail.com>
2022-09-13 11:52:34 -0700

    edited

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,16 +4,16 @@
 *Return: 0
 */
 
-iint main(void)
+int main(void)
 {
-int a=s;
+int a;
 char s = "_putchar";
 for (a = 0; a <= 9; a++)
 
 if (a > 0)
 {
 _putchar(a);
-_putchar('\n');
+_putchar("%s\n",s);
 break;
 }
 return (0);
