ed241fe06787d86d1799694adce2f4f484c3dcc4
omuabormalin <omuabormalin@gmail.com>
2022-09-13 14:42:51 -0700

    edited the file code

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,16 +7,16 @@
 
 int print_last_digit(int n)
 {
-int l;
-l = n % 10;
-if (l < 0)
+int i;
+i = n % 10;
+if (i < 0)
 {
 _putchar(-l + 48);
 return (-l);
 }
 else
-}
-_putchar(l + 48);
-return (l);
+{
+_putchar(i + 48);
+return (1);
 }
 }
