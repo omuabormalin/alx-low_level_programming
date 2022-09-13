49b47b23c7382d8740897a90a5062ac1d2041cee
omuabormalin <omuabormalin@gmail.com>
2022-09-13 13:55:25 -0700

    edited int _islower(int c); file

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*int _islower(int c) - print loewer case
+*Return is zero
+*/
+
+int _islower(int c)
+{
+if (c = "a" && c <= "z")
+{
+return (1);
+}
+else
+{
+return (0);
+}
+_putchar('\n');
+}
