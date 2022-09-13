d3ee615a741f39d44a444ee5bc38f8923ca51c61
omuabormalin <omuabormalin@gmail.com>
2022-09-13 13:09:52 -0700

    edited

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,7 +6,14 @@
 
 int main(void)
 {
-_putchar('%s_putchar');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 _putchar('\n');
 return (0);
 }
