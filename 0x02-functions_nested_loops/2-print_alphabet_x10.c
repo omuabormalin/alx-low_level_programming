91016301dd740ec3da2a3ebc11456cf8d387c67f
omuabormalin <omuabormalin@gmail.com>
2022-09-13 13:42:11 -0700

    edited 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,9 +6,9 @@
 
 void print_alphabet_x10(void)
 {
-int m;
 int w;
-for (w = 0; w <= 10; w++)
+int m;
+for (w = 0; w <= 9; w++)
 {
 for (m = 'a'; m <= 'z'; m++)
 {
@@ -16,5 +16,4 @@ _putchar(m);
 }
 _putchar('\n');
 }
-return (0);
 }
