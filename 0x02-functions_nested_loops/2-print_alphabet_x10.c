636f589044e4c3ff8545d19ea5c454ff2ab59beb
omuabormalin <omuabormalin@gmail.com>
2022-09-13 13:22:14 -0700

    edited 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*print_alphabet - print 10x
+*Return - zero value 0
+*/
+
+void print_alphabet_x10(void);
+{
+int m;
+for (m = 'a'; m <= 'z'; m++)
+_putchar(m * 10);
+_putchar('\n');
+return (0);
+}
