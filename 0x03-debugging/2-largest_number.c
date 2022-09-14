d561ecfa3e6446394619bfc289eaf2ebfeb413b5
omuabormalin <omuabormalin@gmail.com>
2022-09-14 14:47:54 -0700

    edited the 2-largest_number.c file

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-*argest_number - returns the largest of 3 numbers
+*largest_number - returns the largest of 3 numbers
 *@a: first integer
 *@b: second integer
 *@c: third integer
