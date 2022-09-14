581139efcda748cd9a42e509d8cf92455ab962b9
omuabormalin <omuabormalin@gmail.com>
2022-09-14 14:52:31 -0700

    edited the file code 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -9,18 +9,19 @@
 
 int largest_number(int a, int b, int c)
 {
-int largest;
-if (a > b && b > c)
-{
+
+	int largest;
+	if (a >= b && b >= c)
+	{
 	largest = a;
-}
-else if (b > a && a > c)
-{
+	}
+	else if (b >= a && a >= c)
+	{
 	largest = b;
-}
-else
-{
+	}
+	else
+	{
 	largest = c;
-}
-return (largest);
+	}
+	return (largest);
 }
