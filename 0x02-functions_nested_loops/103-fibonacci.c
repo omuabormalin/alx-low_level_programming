31e00239ec4e640d2f0b60a8677375e36ec0aa94
omuabormalin <omuabormalin@gmail.com>
2022-09-14 02:12:53 -0700

    edited the code in file 103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+*main - the main code goes here
+*Return: a zero value
+*/
+int main(void)
+{
+int i
+unsigned ling int a, b, c, d;
+j = 1;
+k = 2;
+m = 0;
+for (i = 1; i <= 33; i++)
+{
+if(j < 4000000 && (j % 2) == 0)
+{
+m = m+ j;
+}
+l = j +k;
+j = k;
+k = l;
+}
+printf("%lu\n",m);
+return (0);
+}
