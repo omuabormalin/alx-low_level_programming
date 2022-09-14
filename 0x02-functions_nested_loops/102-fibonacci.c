7be10cbce267f21613fe3b18c0ad873fa680a37c
omuabormalin <omuabormalin@gmail.com>
2022-09-13 21:10:20 -0700

    edited the codes in file 102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/*
+*main - anyhinh
+*Return: i tire
+*/
+
+intmain(void)
+{
+long int i, j, k, l;
+j = 1;
+k = 2;
+for (i = 1; i <= 50; ++i)
+{
+if (j != 20365011074)
+{
+printf("%ld, ", j);
+}
+else
+{
+printf("%ld\n", j);
+}
+l = j + k;
+j = k;
+k = i;
+}
+return (0);
+}
