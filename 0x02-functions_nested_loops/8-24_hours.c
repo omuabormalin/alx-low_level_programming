73535dff58015d00e9ad961d3f7e1e58fc716de9
omuabormalin <omuabormalin@gmail.com>
2022-09-13 20:10:55 -0700

    edited the file code

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+nclude "main.h"
+/**
+*jack_bauer - prints every minute of the day
+*Return: no return
+*/
+
+void jack_bauer(void)
+{
+int a;
+int b;
+int c;
+int d;
+for (a = 48; a <= 50; a++)
+{
+for (b = 48; b <= 57; b++)
+{
+for (c = 48; c <= 53; c++)
+{
+for (d = 48; d <= 57; d++)
+{
+if (a >= 50 && b >= 52)
+break;
+_putchar(a);
+_putchar(b);
+_putchar(58);
+_putchar(c);
+_putchar(d);
+_putchar('\n');
+}
+}
+}
+}
+}
