3ac42738f559f91d6c7398a52c2c2fc9edb5f87b
omuabormalin <omuabormalin@gmail.com>
2022-09-15 13:40:08 -0700

    i created a 1-isdigit.c file and code in it

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*_isdigit - shows digits
+*@c: holds the value
+*Return: always zero is a success#
+*/
+
+int _isdigit(int c)
+{
+if (c >= 0 && c <= 9)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
