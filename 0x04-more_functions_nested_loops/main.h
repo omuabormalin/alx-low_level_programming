42267a806cac07aaa833054eb15eeb18fd270828
omuabormalin <omuabormalin@gmail.com>
2022-09-15 10:43:49 -0700

    edited the main.h file

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN.H
+#define MAIN.H
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_square(int size);
+void print_triangle(int size);
+int _putchar(char c);
+#endif
