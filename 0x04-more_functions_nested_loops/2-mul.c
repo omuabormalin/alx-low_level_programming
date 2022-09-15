149f1fc974bff749a0e20f06bae350036bdf5975
omuabormalin <omuabormalin@gmail.com>
2022-09-15 16:08:56 -0700

    edited and created 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+*mul - it multiplies 2 numerical values
+*@a: first parameter
+*@b: second parameter
+*Return: product
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
