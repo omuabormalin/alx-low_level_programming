823667ab110f8b0a63a9dff0f7e5b2b0485eea70
omuabormalin <omuabormalin@gmail.com>
2022-09-15 12:26:23 -0700

    edited the main.h file

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,15 @@
 #include "main.h"
+
 /**
-*_isupper - the main code
+*_isupper - checks for upper case letter 
 *@c: the holder
 *Return: 0 if success
 */
+
 int _isupper(int c)
 {
 
-if (c > 65  && c < 91)
+if (c >= 65  && c < 91)
 {
 return (1);
 }
