1a92feaef2238a55a7983ba0f7a73e3e0efe2e34
omuabormalin <omuabormalin@gmail.com>
2022-09-15 10:34:32 -0700

    created and eddited int _isupper(int c) file

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*_isupper - the main code
+*@c: the holder
+*Return: 0
+*/
+int _isupper(int c)
+{
+if (c >= 'A' && C <= 'Z')
+{
+	return (1);
+}
+else
+{
+return (0);
+}
+}
