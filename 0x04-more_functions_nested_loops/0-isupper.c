5473a65f75df10609dd4dd8b85b5f83fb7356463
omuabormalin <omuabormalin@gmail.com>
2022-09-15 11:19:40 -0700

    edited the code file 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,11 +2,12 @@
 /**
 *_isupper - the main code
 *@c: the holder
-*Return: 0
+*Return: 0 if success
 */
 int _isupper(int c)
 {
-if (c >= 'A' && c <= 'Z')
+
+if (c > 65  && c < 91)
 {
 return (1);
 }
