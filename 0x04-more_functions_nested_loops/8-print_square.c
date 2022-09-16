081d6853c31cf990fd31a5846f13a4d081949f8a
omuabormalin <omuabormalin@gmail.com>
2022-09-15 21:05:04 -0700

    edited the file 8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+*print_square - prints a square
+*@size: parameter
+*/
+void print_square(int size)
+{
+
+int i = 0, j;
+if (size > 0)
+{
+for (; i < size; i++)
+{
+for (j = 0; j < size; j++)
+_putchar(35);
+_putchar('\n');
+}
+}
+else
+_putchar('\n');
+}
