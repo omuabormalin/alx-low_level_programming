d0025e3d4acd7815ef9e68d7a8fc91b9ea1b6f94
omuabormalin <omuabormalin@gmail.com>
2022-09-19 12:19:24 -0700

    created and edited 4-print_rev.c file

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - to print in reverse
+ * @s: the var char
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	int j
+
+	while (s[i] != '\0';)
+	{
+		i++
+	}
+	for (j = i - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
+
