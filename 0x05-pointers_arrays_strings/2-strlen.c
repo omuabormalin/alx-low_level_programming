e042efd6594d93b6f1f78d7daa7fdc1ec6c0ad30
omuabormalin <omuabormalin@gmail.com>
2022-09-19 10:51:35 -0700

    created 2-strlen.c file

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*_strlen - gives the lenght of a string
+*@s: var character
+*/
+int _strlen(char *s)
+{
+	int t = 0;
+
+	while (s[t] != '\0')
+	{
+		t++
+	}
+	return (t);
+}
+
