e122ab216b5fb36c5db8debc10f96bccab33f55e
omuabormalin <omuabormalin@gmail.com>
2022-09-20 04:02:17 -0700

    edited and created 8-print_array.c file

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_array - to print an array
+ * @a:pointer variable
+ * @n: variable
+ */
+void print_array(int *a, int n)
+{
+	int x;
+
+	for (x = 0 ; x < n; x++)
+	{
+		printf("%d", a[x]);
+		if (x != n - 1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
