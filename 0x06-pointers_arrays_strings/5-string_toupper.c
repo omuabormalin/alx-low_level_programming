598dd0faa4de817971090f2104531dfa711ee530
omuabormalin <omuabormalin@gmail.com>
2022-09-21 13:04:36 -0700

    created 5-string_toupper.c file

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * string_toupper - function for my code
+ * @str: string
+ * Return: char
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return (str);
+}
