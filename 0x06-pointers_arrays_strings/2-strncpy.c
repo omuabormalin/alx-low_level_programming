19d1b6ee43444ce3cf9dc3aa0dfa4ac3d7f0f73c
omuabormalin <omuabormalin@gmail.com>
2022-09-21 10:12:50 -0700

    created a new file 2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strncpy - copies strings
+ * @dest: a pointer
+ * @src: pointer
+ * @n: variable
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{#
+	int i;
+
+	i = 0;
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
