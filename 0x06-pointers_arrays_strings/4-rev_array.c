7723fc9bae97f9c6d1b26b52a2924ef112f5e900
omuabormalin <omuabormalin@gmail.com>
2022-09-21 12:51:03 -0700

    edited and created 4-rev_array.c file

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * reverse_array - an array in reverse form
+ * @a: a pointer  variable integer
+ * @n: a variable integer
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int *start_c, *end_c, c;
+	int i;
+
+	start_c = a;
+	end_c = a;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		end_c++;
+	}
+	for (i = 0; i < n / 2; i++;)
+	{
+		c = *end_c;
+		*end_c = *start_c;
+		*start_c = c;
+		start_c++;
+		end_c--;
+	}
+}
