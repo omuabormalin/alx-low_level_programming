ea4b2af8fe493a4e120d1f4a796f48749251c689
omuabormalin <omuabormalin@gmail.com>
2022-09-22 09:22:11 -0700

    edited 6-cap_string.c file

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,10 +6,11 @@
  */
 char *cap_string(char *s)
 {
-	int count = 0; i;int separators[] = (32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125);
+	int count = 0, i;
+	int separators[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
 	if (*(s + count) >= 97 && *(s + count) <= 122)
-		*(s + count) = *(s + count) -32;
+		*(s + count) = *(s + count) - 32;
 	count++;
 	while (*(s + count) != '\0')
 	{
