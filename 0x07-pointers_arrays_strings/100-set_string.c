4e1cb2da773bad603774991b861ffdd792f71c6c
omuabormalin <omuabormalin@gmail.com>
2022-09-26 13:19:07 -0700

    tast 100

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: content to be copied
+ * @to: string
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
