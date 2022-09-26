d1a411344148622caa668e79f615996476e3936f
omuabormalin <omuabormalin@gmail.com>
2022-09-26 13:01:19 -0700

    edited 4-strpbrk.c file

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,20 +11,20 @@ char *_strpbrk(char *s, char *accept)
 
 {
 
-	int j;
+int j;
 
-	while (*s != '\0')
-	{
-		j = 0;
-		while (accept[j] != '\0')
-			[
-				if (*s == accept[j])
-				{
-					return (s);
-				}
-		j++;
-	}
-	s++;
+while (*s != '\0')
+{
+j = 0;
+while (accept[j] != '\0')
+{
+if (*s == accept[j])
+{
+return (s);
+}
+j++;
+}
+s++;
 }
 return (0);
-
+}
