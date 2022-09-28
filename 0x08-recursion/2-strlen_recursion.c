019656a31b314d016327d421218aaba1dc719dcf
omuabormalin <omuabormalin@gmail.com>
2022-09-28 09:31:14 -0700

    created 2-strlen_recursion.c file

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen_recursion - length of a string
+ * @s: pointer block of memory to fill
+ * Return: strlen_recursion
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
+
