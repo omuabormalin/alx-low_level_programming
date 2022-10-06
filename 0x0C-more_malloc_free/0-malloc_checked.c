9a27629244f89b55dc6dcfaa3d29c3ceb158a703
omuabormalin <omuabormalin@gmail.com>
2022-10-06 08:55:20 -0700

    edited 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -7,27 +7,16 @@
  *   *@b: number of memory
  *
  *    *Return: void
- *
  *     */
 
 void *malloc_checked(unsigned int b)
-
 {
+void *p;
 
-		void *p;
-
-
-
-			p = malloc(b);
-
-				if (p == NULL)
-
-						{
-
-									exit(98);
-
-										}
-
-					return (p);
-
+p = malloc(b);
+if (p == NULL)
+{
+exit(98);
+}
+return (p);
 }
