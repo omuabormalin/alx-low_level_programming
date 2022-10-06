bf66ec116cc13a1c8bf3638b4a8f9d4e2a2fc0ca
omuabormalin <omuabormalin@gmail.com>
2022-10-06 09:02:24 -0700

    was edited

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,13 +1,9 @@
 #include "main.h"
-
 /**
- *
- *  *malloc_checked - array for prints a string
- *
- *   *@b: number of memory
- *
- *    *Return: void
- *     */
+*malloc_checked - array for prints a string
+*@b: number of memory
+*Return: void
+*/
 void *malloc_checked(unsigned int b)
 {
 void *p;
