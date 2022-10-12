fc6ecee59fc76d937fd0ff044e19b21d87cd06f5
omuabormalin <omuabormalin@gmail.com>
2022-10-12 07:08:02 -0700

    edited 2-main.c

--- 0x0F-function_pointers/2-main.c
+++ 0x0F-function_pointers/2-main.c
@@ -15,6 +15,7 @@ int is_98(int elem)
  * Return: 0 if false, something else otherwise
  */
 int is_strictly_positive(int elem)
+
 {
 	return (elem > 0);
 }
@@ -24,6 +25,7 @@ int is_strictly_positive(int elem)
  * Return: 0 if false, something else otherwise
  */
 int abs_is_98(int elem)
+
 {
 	return (elem == 98 || -elem == 98);
 }
@@ -34,13 +36,14 @@ int abs_is_98(int elem)
 int main(void)
 {
 	int array[10] = {0, -98, 98, 402, 1024, 4096, -1024, -98, 1, 2};
-		int array[10] = {3, 4, 5, 6, 7, 8, 9, 10, 11, 98};
-			int index
-				index = int_index(array, 20, is_98);
-				printf("%d\n", index);
-					index = int_index(array, 20, abs_is_98);
-						printf("%d\n", index);
-							index = int_index(array, 20, is_strictly_positive);
-								printf("%d\n", index);
-									return (0);
+	int array[10] = {3, 4, 5, 6, 7, 8, 9, 10, 11, 98};
+	int index
+
+		index = int_index(array, 20, is_98);
+		printf("%d\n", index);
+		index = int_index(array, 20, abs_is_98);
+		printf("%d\n", index);
+		index = int_index(array, 20, is_strictly_positive);
+		printf("%d\n", index);
+		return (0);
 }
