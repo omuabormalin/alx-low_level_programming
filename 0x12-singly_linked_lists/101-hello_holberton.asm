2f176e81cdebfa0c1bd8b57b9ec689221f029ef0
omuabormalin <omuabormalin@gmail.com>
2022-10-20 10:50:47 -0700

    changed d code

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,11 +1,41 @@
-#include <stdio.h>
-/**
-* main - holds my main code
-* Return: no return value
-*/
+extern printf
 
-int main(void)
-{
-printf("Hello, Holberton");
-return (0);
-}
+
+
+section .text
+
+   global main
+
+
+
+main:
+
+   push rbp
+
+
+
+   mov rdi,fmt
+
+   mov rsi,msg
+
+   mov rax,0
+
+   call printf
+
+
+
+   pop rbp
+
+
+
+   mov rax,0
+
+   ret
+
+
+
+section .data
+
+   msg: db "Hello, Holberton", 0
+
+   fmt: db "%s", 10, 0
