ed3e751468928b7fa56d3d38a6b15aa473c40b1a
omuabormalin <omuabormalin@gmail.com>
2022-10-20 09:12:59 -0700

    task four

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -17,6 +17,7 @@ typedef strut list_s
 	unsigned int len;
 	struct list_s *next;
 }list_t;
+
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
