1b50f3ebc74259504a3f0846159fb23e9ba1cfcf
omuabormalin <omuabormalin@gmail.com>
2022-10-25 05:27:39 -0700

    created files for task 0 to 5

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * listint_len - Returns the number of elements
+ * @h: A pointer to the head of the listint_t list.
+ * Return: The number of elements in the listint_t list.
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+	return (nodes);
+}
