05f728a82806e4f22546d84b8a219e7e1e784d09
omuabormalin <omuabormalin@gmail.com>
2022-11-01 12:35:29 -0700

    i edited task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,38 +1,28 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * read_textfile - Reads a text file and prints it to POSIX stdout.
- * @filename: A pointer to the name of the file.
- * @letters: The number of letters the
- * function should read and print.
- * Return: If the function fails or filename is NULL - 0.
- *  O/w - the actual number of bytes the function can read and print.
+ * read_textfile - reads textfiles
+ * @filename: filename
+ * @letters: number of printed letters
+ * Return: number of printed letters. if it fails, return 0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t o, r, w;
-	char *buffer;
+	int xx;
+	ssize_t nrd, nwr;
+	char *buf;
 
-	if (filename == NULL)
+	if (!filename)
 		return (0);
 
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
+	xx = open(filename, O_RDONLY);
+	if (xx == -1)
 		return (0);
-	o = open(filename, O_RDONLY);
-	r = read(o, buffer, letters);
-	w = write(STDOUT_FILENO, buffer, r);
-
-	if (o == -1 || r == -1 || w == -1 || w != r)
-	{
-		free(buffer);
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
 		return (0);
-	}
-	
-	free(buffer);
-	close(0);
-
-
-	return (w);
+	nrd = read(xx, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
+	close(xx);
+	free(buf);
+	return (nwr);
 }
